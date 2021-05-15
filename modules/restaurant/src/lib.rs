5899c74a3085592482400ad390a207f3819d6cca
Nargonath <jonas.pauthier@gmail.com>
2021-05-15 13:12:41 +0200

    Split restaurants examples into separate files

--- modules/restaurant/src/lib.rs
+++ modules/restaurant/src/lib.rs
@@ -1,6 +1,8 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
+mod front_of_house;
+
 mod back_of_house {
     pub struct Breakfast {
         pub toast: String,
@@ -22,12 +24,6 @@ mod back_of_house {
     }
 }
 
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-    }
-}
-
 pub fn eat_at_restaurant() {
     let mut meal = back_of_house::Breakfast::summer("Rye");
     meal.toast = String::from("Wheat");
@@ -38,4 +34,7 @@ pub fn eat_at_restaurant() {
 
     let order1 = back_of_house::Appetizer::Soup;
     let order2 = back_of_house::Appetizer::Salad;
+
+    // use code from the module included in file front_of_house.rs
+    front_of_house::hosting::add_to_waitlist();
 }
