066cd92d5e0e0302958f1ecfaee53121db993f88
Nargonath <jonas.pauthier@gmail.com>
2021-05-15 12:46:50 +0200

    Add public struct/enum examples

--- modules/restaurant/src/lib.rs
+++ modules/restaurant/src/lib.rs
@@ -1,3 +1,27 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+
 mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {}
@@ -5,7 +29,13 @@ mod front_of_house {
 }
 
 pub fn eat_at_restaurant() {
-    crate::front_of_house::hosting::add_to_waitlist();
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
 
-    front_of_house::hosting::add_to_waitlist();
+    // this won't work since the field is private
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
