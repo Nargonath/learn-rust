8b0f4b1499f0820de06b65e1ed6577d0fb45c1a1
Nargonath <jonas.pauthier@gmail.com>
2021-05-31 15:39:31 +0200

    Add pig-latin exercise

---
+++ pig-latin/src/main.rs
@@ -0,0 +1,35 @@
+fn is_in_array(haystack: &[char], needle: Option<char>) -> bool {
+    match needle {
+        None => false,
+        Some(ndl) => {
+            for c in haystack {
+                if *c == ndl {
+                    return true;
+                }
+            }
+            false
+        }
+    }
+}
+
+fn convert_to_pig_latin(string: &String) -> String {
+    let vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+    let first_char = string.chars().nth(0);
+    let start_with_vowel = is_in_array(&vowels, first_char);
+
+    if start_with_vowel {
+        format!("{}-hay", string)
+    } else {
+        match first_char {
+            None => String::from(""),
+            Some(c) => format!("{}-{}ay", &string[1..], &c),
+        }
+    }
+}
+
+fn main() {
+    let test = String::from("car");
+
+    println!("initial string = {}", &test);
+    println!("translated string = {}", &convert_to_pig_latin(&test));
+}
