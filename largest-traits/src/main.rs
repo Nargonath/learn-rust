3c0e2ccef5be45ea1411cd9908e8de2d6eff433c
Nargonath <jonas.pauthier@gmail.com>
2021-06-09 19:07:54 +0200

    Add largest traits variant exercise

---
+++ largest-traits/src/main.rs
@@ -0,0 +1,27 @@
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest_clone<T: PartialOrd + Clone>(list: &[T]) -> T {
+    let mut largest = list[0].clone();
+
+    for item in list {
+        if *item > largest {
+            largest = item.clone();
+        }
+    }
+
+    largest
+}
+
+fn main() {
+    println!("Hello, world!");
+}
