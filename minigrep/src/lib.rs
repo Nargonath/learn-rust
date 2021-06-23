990bfb009c07d74334fb02733a73be7751fe9398
Nargonath <jonas.pauthier@gmail.com>
2021-06-23 10:41:56 +0200

    minigrep: add test and functional implementation of search_insensitive

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -42,6 +42,19 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     needles
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -89,13 +102,29 @@ mod tests {
     }
 
     #[test]
-    fn search_success() {
+    fn search_sensitive_success() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn search_insensitive_success() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
