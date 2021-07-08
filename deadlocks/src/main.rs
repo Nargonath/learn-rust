a0a37fed85b33fed74196248509f956d01ccc78e
Nargonath <jonas.pauthier@gmail.com>
2021-07-08 10:59:43 +0200

    Add mitigation for the deadlock example

--- deadlocks/src/main.rs
+++ deadlocks/src/main.rs
@@ -11,24 +11,30 @@ fn main() {
         let mut num = counter_temp_1.lock().unwrap();
         *num += 1;
 
+        let result = *num;
+        drop(num);
+
         // gives enough time for the other thread to start
         // and lock
         thread::sleep(time::Duration::from_millis(3000));
 
         let num2 = counter_temp_2.lock().unwrap();
-        println!("Total thread1 = {}", *num + *num2);
+        println!("Total thread1 = {}", result + *num2);
     });
 
     let handle2 = thread::spawn(move || {
         let mut num2 = counter2.lock().unwrap();
         *num2 += 1;
 
+        let result = *num2;
+        drop(num2);
+
         // gives enough time for the other thread to start
         // and lock
         thread::sleep(time::Duration::from_millis(3000));
 
         let num = counter1.lock().unwrap();
-        println!("Total thread2 = {}", *num + *num2);
+        println!("Total thread2 = {}", *num + result);
     });
 
     handle.join().unwrap();
